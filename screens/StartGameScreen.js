1f0137e6d1aae399ba0bfff12a9e36d533c051bd
Alexis Delgado <dalexisbr@gmail.com>
2020-11-05 16:55:23 -0700

    Creating and Adding style to Input component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
 import Card from '../components/Card'
+import Input from '../components/Input'
 import Colors from '../constants/colors'
 
 const StartGameScreen = props => {
@@ -10,7 +11,12 @@ const StartGameScreen = props => {
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input style={styles.input}
+          blurOnSubmit autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='phone-pad'
+          maxLength={2} 
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title="Reset" onPress={() => {}} color={Colors.secondary}/>
@@ -47,6 +53,10 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
   }
 })
 
