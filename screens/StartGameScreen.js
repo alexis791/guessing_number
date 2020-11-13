81c36907d8a34697dc5b6b816db4c3bee7c8fc22
Alexis Delgado <dalexisbr@gmail.com>
2020-11-13 11:05:12 -0700

    Custom Fonts

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,6 +5,8 @@ import Card from '../components/Card'
 import Input from '../components/Input'
 import NumberContainer from '../components/NumberContainer'
 import Colors from '../constants/colors'
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 
 const StartGameScreen = props => {
 
@@ -42,7 +44,7 @@ const StartGameScreen = props => {
   if(confirmed) {
     confirmedOutput = (
       <Card style={styles.summaryContainer}>
-        <Text>You selected</Text>
+        <BodyText>You selected</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
         <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
       </Card>
@@ -56,9 +58,9 @@ const StartGameScreen = props => {
       Keyboard.dismiss()
     }}>
       <View style={styles.screen}>
-        <Text style={styles.title}>Start a New Game!</Text>
+        <TitleText style={styles.title}>Start a New Game!</TitleText>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input style={styles.input}
             blurOnSubmit autoCapitalize='none'
             autoCorrect={false}
@@ -91,6 +93,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     marginVertical: 10,
+    fontFamily:'open-sans-bold'
   },
   inputContainer:{
     width: 300,
